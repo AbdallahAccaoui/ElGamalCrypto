37be329a2b25d35b9c573cf1095e3b9729ee8a88
Abdallah Accaoui <abdallah_accaoui@hotmail.com>
2021-06-07 00:53:59 +0300

    Initial Commit

---
+++ src/elgamal/Elgamal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elgamal;
+
+/**
+ *
+ * @author HP
+ */
+public class Elgamal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ElGamalForm form= new ElGamalForm();
+        form.setVisible(true);
+    }
+    
+}
